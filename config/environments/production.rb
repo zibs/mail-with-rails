1f8292d0072ce11bd1dc1c86d671a9e886dae54f
zibs <elizibin@gmail.com>
2015-08-30 19:30:37 -0700

    prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,16 +23,16 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   # host = '<your heroku app>.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+  # config.action_mailer.default_url_options = { host: host }
+  # ActionMailer::Base.smtp_settings = {
+    # :address        => 'smtp.sendgrid.net',
+    # :port           => '587',
+    # :authentication => :plain,
+    # :user_name      => ENV['SENDGRID_USERNAME'],
+    # :password       => ENV['SENDGRID_PASSWORD'],
     # :domain         => '.com',
-    :enable_starttls_auto => true
-  }
+    # :enable_starttls_auto => true
+  # }
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
